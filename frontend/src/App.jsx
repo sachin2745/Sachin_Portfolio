9d7c20fa1752703c691ac89b089e2a5bb459e3b0
sachin2745 <sachin.official648@gmail.com>
2024-06-22 13:28:18 +0530

    about me page designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,14 @@ import Project from './components/Project';
 import Experience from './components/Experience';
 import Experiences from './components/Experiences';
 import HireMe from './components/HireMe';
+import Aboutme from './components/Aboutme';
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Aboutme />
       <HireMe />
       <Experiences />
       {/* <Experience /> */}
---
+++ frontend/src/components/Aboutme.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './aboutme.css'
+
+const Aboutme = () => {
+    return (
+        <section className='sticky top-0 bg-tertiary'>
+            <div className=" body-font pb-40">
+                <div className="container  mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                    <div className="flex lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+
+                        <h1 className="title-font text-center rotate-180 [writing-mode:_vertical-lr]  sm:text-4xl text-3xl mb-4 font-Rammetto_One text-white">
+                            About  <span className='text-primary  '>Me !</span>
+                        </h1>
+
+                        <img
+                            className="object-cover  grayscale hover:grayscale-0 aspect-square"
+                            alt="hero"
+                            src="mypic.png "
+                        />
+                    </div>
+
+                    <div className="pt-20 lg:flex-grow md:w-1/2  lg:pl-40 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                        <h1 className="title-font  sm:text-4xl text-3xl mb-4 font-Rammetto_One text-white">
+                            Know Who  I  <span className='text-primary '>Am</span>     <span className='text-primary e font-bold text-5xl'>!</span>
+                        </h1>
+                        <p className="mb-4 font-Josefin_Sans text-lg leading-relaxed text-white max-w-screen-sm">
+                            <span className='text-primary font-Jost font-bold '>Sachin Kumar</span>, a recent  <span className='text-primary font-Jost font-bold '>Computer Applications graduate</span>, is here to make a splash in the IT sector!
+                            My passion for technology is undeniable, and I'm eager to leverage my fresh perspective and diverse skillset to propel a company forward.
+                            With a strong foundation in  <span className='text-primary font-Jost  font-bold l'>MERN Stack Development</span> (React.js, Node.js, Express.js, MongoDB), I'm not just proficient – I'm obsessed with crafting user-centric web experiences that captivate and engage.
+                        </p>
+                        <p className='text-white font-Josefin_Sans text-lg max-w-screen-sm'>
+                            But my talents extend beyond code. I possess SEO expertise and design prowess with Canva, making me a well-rounded asset for any team.
+                        </p>
+                        <p className=' font-Josefin_Sans text-lg text-white mt-2 max-w-screen-sm'>
+                            Are you ready to create something extraordinary together? Let's collaborate and turn innovative ideas into impactful  <span className='text-primary font-Jost font-bold '>realities!</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Aboutme
